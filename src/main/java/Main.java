b058086f6bfb5b2cfb63e783d7839e1fad6a5d8c
Vasilisa Nichipor <vsilfox@gmail.com>
2022-11-03 14:28:10 +0300

    Fix exceptions catching in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     final static int NUMBER_OF_THREADS = 4;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         ThreadGroup threadGroup = new ThreadGroup("thread group");
         Runnable runnable = new SleepingAndSpeakingThread();
@@ -10,7 +10,11 @@ public class Main {
         for (int i = 0; i < NUMBER_OF_THREADS; i++) {
             new Thread(threadGroup, runnable, "Thread " + (i + 1)).start();
         }
-        Thread.sleep(8000);
+        try {
+            Thread.sleep(8000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         threadGroup.interrupt();
     }
 }
